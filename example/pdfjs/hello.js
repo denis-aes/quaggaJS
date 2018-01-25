b45f46021f223199de8b767060e0c495470d9ac3
denis.obydennykh <dobydennykh@griddynamics.com>
2018-01-25 13:27:36 +0300

    style optimization

--- example/pdfjs/hello.js
+++ example/pdfjs/hello.js
@@ -1,30 +1,43 @@
 'use strict';
 
-PDFJS.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.290/pdf.worker.js';
+function render(){
+    let href = document.location.href;
+    if (href.indexOf('?usePdf') > -1){
+        renderPdf();
+    }
+    else{
+        document.getElementById('the-image').style.display = 'block';
+        runQuagga('barcode.png');
+    }
+}
+
+function renderPdf(){
+    document.getElementById('the-canvas').style.display = 'block';
 
 // Fetch the PDF document from the URL using promises.
-PDFJS.getDocument('barcode.pdf').then(function (pdf) {
-    // Fetch the page.
-    pdf.getPage(1).then(function (page) {
-        var scale = 1.5;
-        var viewport = page.getViewport(scale);
+    PDFJS.getDocument('barcode.pdf').then(function (pdf) {
+        // Fetch the page.
+        pdf.getPage(1).then(function (page) {
+            var scale = 1.5;
+            var viewport = page.getViewport(scale);
 
-        // Prepare canvas using PDF page dimensions.
-        var canvas = document.getElementById('the-canvas');
-        var context = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
+            // Prepare canvas using PDF page dimensions.
+            var canvas = document.getElementById('the-canvas');
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
 
-        // Render PDF page into canvas context.
-        var renderContext = {
-            canvasContext: context,
-            viewport: viewport
-        };
-        page.render(renderContext);
+            // Render PDF page into canvas context.
+            var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+            page.render(renderContext);
 
-        runQuagga(canvas.toDataURL());
+            runQuagga(canvas.toDataURL());
+        });
     });
-});
+}
 
 function runQuagga(imageSrc) {
 
